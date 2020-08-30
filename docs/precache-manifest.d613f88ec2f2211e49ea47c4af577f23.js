a1273795754c7ce74d52e86a96d97785f740a5eb
Jairo Moreno <jairotbg@hotmail.com>
2020-08-29 21:21:27 -0400

    docs agregado

---
+++ docs/precache-manifest.d613f88ec2f2211e49ea47c4af577f23.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad24761982037dea54b528f576beff00",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8e83ecf7b4aa6fd36912",
+    "url": "/static/css/main.16dd1260.chunk.css"
+  },
+  {
+    "revision": "ce22bc40f56663d43cc7",
+    "url": "/static/js/2.2eda30d3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2eda30d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e83ecf7b4aa6fd36912",
+    "url": "/static/js/main.33dd31ae.chunk.js"
+  },
+  {
+    "revision": "0a62db85d9147c438712",
+    "url": "/static/js/runtime-main.2bd8def6.js"
+  }
+]);
